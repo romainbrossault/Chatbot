e633a6a9a2283a722ee4869b1a5b75e063dfe318
noahwojciechowski <noah.wojciechowski@stfelixlasalle.fr>
2025-02-07 09:58:09 +0100

    Page Chat

--- Frontend/frontend/src/Pages/Chats.js
+++ Frontend/frontend/src/Pages/Chats.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles/chats.css";
 
 export const Chats = () => {
-    return <div>Mes Chats</div>;
-};
+   const [message, setMessage] = useState("");
+   
+       const handleSendMessage = () => {
+           // Logic to send message to chatbot
+           console.log("Message sent to chatbot:", message);
+       };
+   
+       return (
+           <div className="container">
+               <div className="textarea-container">
+                   <textarea
+                       placeholder="Chat#1 Title"
+                       value={message}
+                       onChange={(e) => setMessage(e.target.value)}
+                   />
+                   <div className="creation-date">Date de création : 07/02/25</div>
+                   <div className="modify-date">Date de modification : 07/02/2025</div>
+                   <br />
+                    <button className='modify-button' onClick={handleSendMessage}>Modify</button>
+                    <button className='delete-button' onClick={handleSendMessage}>Delete</button>
+               </div>
+           </div>
+       );
+   };
 
 export default Chats;
